5141666f181e6c7b922ae815c75e340a910a7860
shamimrana2006 <mdshamimrana20@gmail.com>
2024-08-02 01:49:15 +0600

    new mern repo create backend server with vercel

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 
-app.listen(3000,()=>{
-    console.log("server is running on port http://localhost:300");
+app.listen(3045,()=>{
+    console.log("server is running on port http://localhost:3045");
 })
