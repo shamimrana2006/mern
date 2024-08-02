e56baeed689547c2df13a142a41a5c936de9658d
shamimrana2006 <mdshamimrana20@gmail.com>
2024-08-03 05:50:24 +0600

    add frontend and run with some text

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home({shamim}) {
+  return (
+    <div>
+      <h1 className="text-6xl capitalize text-red-400 text-center h-screen flex items-center justify-center">
+
+        {shamim}
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
