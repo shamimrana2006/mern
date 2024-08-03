e001ee475221b2982e9045af8a2bdd79bb337f03
shamimrana2006 <mdshamimrana20@gmail.com>
2024-08-03 06:33:33 +0600

    add from data i am backend

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.use((req,res,next)=>{
 })
 
 app.use((err,req,res,next) =>{
-    res.json({"error shamim  : " : err})
+    res.json({"error shamim  : " : err.message})
 })
 
 module.exports = app 
