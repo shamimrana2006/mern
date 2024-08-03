62440efcec8e95bc6cf47a539b355dacbc101e0c
shamimrana2006 <mdshamimrana20@gmail.com>
2024-08-03 06:24:32 +0600

    add backend data

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-function Home({shamim}) {
+function Home({ shamim }) {
+  const [name, setName] = useState("");
+  const render = () => {
+    console.log("server data: ", name);
+  };
+  useEffect(async () => {
+    await axios.get("https://mern-backend-azure-tau.vercel.app").then((res) => {
+      return setName(res.data.name);
+    });
+    return;
+  }, []);
+
   return (
     <div>
-      <h1 className="text-6xl capitalize text-red-400 text-center h-screen flex items-center justify-center">
+      <div className="text-6xl capitalize text-red-400 text-center h-screen flex flex-col gap-6 items-center justify-center">
+        {name}
 
-        {shamim}
-      </h1>
+        <button
+          onClick={render}
+          className="rounded bg-green-400 text-center text-sm py-3 px-4"
+        >
+          Render
+        </button>
+
+        <form action="https://mern-backend-azure-tau.vercel.app" method="post">
+          <input type="text" name="name" placeholder="type your name" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
